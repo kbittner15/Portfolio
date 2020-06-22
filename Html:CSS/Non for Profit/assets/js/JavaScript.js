9a1e04a4144ca3862bacf4c30b13f5bd000ce1b1
kbittner15 <61249059+kbittner15@users.noreply.github.com>
2020-06-21 20:53:57 -0400

    HTML/CSS

---
+++ Html:CSS/Non for Profit/assets/js/JavaScript.js
@@ -0,0 +1,64 @@
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+var firstName = "";
+		var lastName = "";
+		var email = "";
+		var dType = "";
+		var receipt = "";
+		var anon = "";
+		var list = "";
+		var amount = "";
+		
+		$('.set-amount').autoGrow(0);
+		
+				
+		$(".button").click(function(){
+			$(".button").removeClass("selected");
+			$(this).addClass("selected");
+			
+			$(this).find("input").focus();
+		});
+		
+		
+		$(".set-amount").keyup(function(){
+			
+			if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+		       this.value = this.value.replace(/[^0-9\.]/g, '');
+		    }
+
+		});
+		
+		
+		$("input").on("change", function(){
+			
+			
+			firstName = $("#firstName").val();
+			lastName = $("#lastName").val();
+			email = $("#email").val();
+			
+			if ( $("#one-time").prop( "checked" ) ){
+				dType = "One-Time";
+			}
+			if ( $("#monthly").prop( "checked" ) ){
+				dType = "Monthly";
+			}
+
+		});
